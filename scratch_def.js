a39e2cff2d9ad3afef67e8a8c6456156c868f4ef
nageshkotari49-lgtm <nageshkotari49@gmail.com>
2026-07-19 20:02:19 +0530

    Delete scratch_def.js

--- scratch_def.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let js = fs.readFileSync('public/script.js', 'utf8');
-
-js = js.replace('showTab("editor");', 'showTab("dashboard");');
-
-fs.writeFileSync('public/script.js', js);
-console.log("Updated default tab");
